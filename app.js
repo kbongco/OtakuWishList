e4b1e245399a062afb48f6197b1c12c0fafd3f5c
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-10-12 16:55:37 -0400

    api showing data

--- app.js
+++ app.js
@@ -5,3 +5,30 @@ let toggleNav = document.getElementById('js-navbar')
 toggleNav.addEventListener('click', function () {
   jsMenu.classList.toggle('active')
 })
+
+topURL = "https://api.jikan.moe/v3/top/anime/1/airing"
+
+const topAnime = (animes) => {
+  animes.forEach((anime) => {
+    let carousel = document.createElement('div')
+    let poster = document.createElement('img')
+    carousel.classList.add('carousel')
+    poster.src = anime.image_url
+    poster.classList.add('carousel__photo')
+    carousel.appendChild(poster)
+    document.querySelector('.carousel-wrapper').append(carousel)
+  })
+}
+
+const getTop = async () => {
+  const topFifty = topURL
+  try {
+    const response = await axios.get(topFifty)
+    topAnime(response.data.top)
+    console.log(response)
+  } catch (error) {
+    console.error(`This is my ${error}`)
+  }
+}
+
+getTop();
