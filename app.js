2bd2f36c40adb60a97359bbbba6492cd0fb8f766
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-10-12 12:35:49 -0400

    added functionality to navbar

---
+++ app.js
@@ -0,0 +1,7 @@
+let jsMenu = document.getElementById('js')
+
+let toggleNav = document.getElementById('js-navbar')
+
+toggleNav.addEventListener('click', function () {
+  jsMenu.classList.toggle('active')
+})
